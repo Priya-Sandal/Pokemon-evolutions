9a74c980d7f1a6a3c8cdae865229017a06d1171a
unknown <add>
2022-10-07 10:38:37 +0530

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <h3>hello HelloWorld</h3> -->
+  {{pokemons}}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {reactive} from "vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'Home',
+  setup(){
+   const state = reactive({
+    pokemons: [],
+    urlIdLookup: {}
+   })
+
+
+
+
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+    .then((res)=> res.json())
+    .then((data)=>{
+     console.log(data)
+     state.pokemons = data.results;
+     state.urlIdLookup = data.results.reduce((acc,curr, index) =>
+     acc = { acc, [curr.name]: index+1},{})
+    })
+    return {...toRefs(state)}
   }
 }
 </script>
